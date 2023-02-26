00fd59db78aea99484b3ca55dc7291c2a3316b8b
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-02-26 23:47:05 +0100

    detail page using Product data from mongodb, addToCartHandler with Product data using axios, added better UX

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -1,8 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
+import axios from 'axios';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { toast } from 'react-toastify';
+import { Store } from '../utils/Store';
 
 export default function ProductItem({ product }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry. Product is out of stock');
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product added to the cart');
+  };
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
@@ -19,7 +37,11 @@ export default function ProductItem({ product }) {
         </Link>
         <p className="mb-3">{product.brand}</p>
         <p className="">$ {product.price}</p>
-        <button className="primary-button bg-amber-300" type="button">
+        <button
+          onClick={() => addToCartHandler(product)}
+          className="primary-button bg-amber-300"
+          type="button"
+        >
           Add to Cart
         </button>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
 //import { Inter } from '@next/font/google';
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
-import data from '../utils/data';
+import Product from '../models/Product';
+import db from '../utils/db';
 
 //const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <>
-      <Layout title={'Homepage'}>
+      <Layout title="Homepage">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {data.products.map((product) => (
-            <ProductItem product={product} key={product.slug} />
+          {products.map((product) => (
+            <ProductItem product={product} key={product.slug}></ProductItem>
           ))}
         </div>
       </Layout>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
