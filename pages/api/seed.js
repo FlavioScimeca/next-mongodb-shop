ef305319f64742d36a006254aaeab79fa41bb4e3
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-02-26 22:37:53 +0100

    seed mongodb with products data

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,11 +1,14 @@
 import User from '../../Models/User';
 import db from '../../utils/db';
 import data from '../../utils/data';
+import Product from '../../models/Product';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded succesfully' });
 };
